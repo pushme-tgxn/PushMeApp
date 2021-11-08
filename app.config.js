e8a5296a062b17c81267c0c5c195deba5b21bd7a
Domenic Horner <domenic@tgxn.net>
2021-11-08 17:04:29 +0800

    add files

--- app.config.js
+++ app.config.js
@@ -3,9 +3,6 @@ export default ({ config }) => {
         ...config,
         extra: {
             backendURL: process.env.BACKEND_URL || "https://pushme.tgxn.net",
-            googleClientId:
-                process.env.GOOGLE_CLIENT_ID ||
-                "496431691586-9g6qno92idps781s2sto66ar863c8jr4.apps.googleusercontent.com",
         },
     };
 };
