a73e146e18ef2a4cc3e2bce2805b71c0de8ef7cf
Domenic Horner <domenic@tgxn.net>
2022-12-07 12:05:54 +0800

    update, add system ui

--- app.config.js
+++ app.config.js
@@ -1,16 +1,21 @@
 export default ({ config }) => {
     return {
         ...config,
-        android: {
-            ...config.android,
-            googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
-        },
         extra: {
             BACKEND_URL: process.env.BACKEND_URL || "https://pushme.tgxn.net",
             eas: {
                 projectId: "dc94d550-9538-48ff-b051-43562cdcf34e",
             },
         },
+        ios: {
+            ...config.ios,
+            googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+        },
+        android: {
+            ...config.android,
+            googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+        },
+
         updates: {
             url: "https://u.expo.dev/dc94d550-9538-48ff-b051-43562cdcf34e",
             fallbackToCacheTimeout: 0,
