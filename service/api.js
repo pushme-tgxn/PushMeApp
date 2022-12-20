66c5dadd40b75eb7321dab0c32522af430d21194
Domenic Horner <domenic@tgxn.net>
2022-12-20 23:11:52 +0800

    update api

--- service/api.js
+++ service/api.js
@@ -4,18 +4,13 @@ import { BACKEND_URL } from "../const";
 
 import APIService from "@pushme-tgxn/pushmesdk";
 
-function createApiService() {
-    const apiService = new APIService({
-        backendUrl: BACKEND_URL,
-        // logging: (message) => {
-        //     console.log(message);
-        //     showToast(message);
-        // },
-    });
-    console.log("createApiService", apiService.backendUrl);
+const apiService = new APIService({
+    backendUrl: BACKEND_URL,
+    // logging: (message) => {
+    //     console.log(message);
+    //     showToast(message);
+    // },
+});
+console.log("createApiService", apiService.backendUrl);
 
-    return apiService;
-}
-
-const apiService = createApiService();
 export default apiService;
